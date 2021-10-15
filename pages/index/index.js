f2662ff113939178ded382ecb27115b1c1551400
meng <1059093241@qq.com>
2021-10-15 22:27:56 +0800

    添加轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,37 +3,19 @@
 const app = getApp()
 
 Page({
-  data: {
-   
+  data:{
+    swiperList:[],
+    data:[1,2,3]
   },
-  // 事件处理函数
-  
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
+  onLoad(){
+    //发送异步请求获取轮播图数据
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (res)=>{
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            swiperList:res.data.message
         })
       }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })

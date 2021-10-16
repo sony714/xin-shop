e87b503a6c023e2c221e871fef44d823e7a38c19
meng <1059093241@qq.com>
2021-10-17 03:35:18 +0800

    楼层样式写完

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,36 @@ import {request} from '../../request/index.js'
 Page({
   data:{
     swiperList:[],
-    data:[1,2,3]
+    catesList:[],
+    floorList:[]
   },
   onLoad(){
+    this.getSwiperList()
+    this.getCatesList()
+    this.getFloorList()
+  },
+  getSwiperList(){
     request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
     .then(res=>{
       this.setData({
         swiperList:res.data.message
+      })
+    })
+  },
+  getCatesList(){
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+    .then(res=>{
+      this.setData({
+        catesList:res.data.message
+      })
+    })
+  },
+  getFloorList(){
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'})
+    .then(res=>{
+      console.log(res)
+      this.setData({
+        floorList:res.data.message
       })
     })
   }

715a7e65ebb5767e5e9973cf9ec5ce17e3dd2f88
meng <1059093241@qq.com>
2021-10-28 22:08:59 +0800

    添加全局加载效果

--- request/index.js
+++ request/index.js
@@ -1,4 +1,12 @@
+//同时发送异步代码次数
+let ajaxTimes = 0
 export const request = (params)=>{
+    ajaxTimes++
+    //显示加载中效果
+    wx.showLoading({
+        title:'加载中',
+        mask: true
+    })
     const BaseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
     return new Promise((resolve,reject)=>{
        wx.request({
@@ -9,6 +17,14 @@ export const request = (params)=>{
           },
           fail:()=>{
               reject(err)
+          },
+          complete:()=>{
+               //关闭图标 
+              ajaxTimes--;
+              console.log(ajaxTimes)
+              if(ajaxTimes==0){
+                wx.hideLoading();
+              }
           }
        })
     })

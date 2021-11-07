96ab5a80b65accab8aaadffe20a19f737bf717a5
meng <1059093241@qq.com>
2021-11-07 21:47:52 +0800

    完成获取收获地址逻辑

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,13 +1,55 @@
 // pages/shop/index.js
+/* 
+ 1.获取用户的收货地址
+  1.绑定点击事件
+  2.调用小程序内置api 获取用户的收获地址 wx.chooseAddress
+  
+  3.获取用户对小程序所授予获取地址的权限状态 scope
+   1.假设用户点击获取收获地址的提示框 确定 authSetting scope.address
+   scope的值为true
+   2.假设用户点击 获取收获地址的提示框 取消
+   scope为 false
+   3.从来没有调用过收获地址的api
+   scope的值为undefined
+*/
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleChooseAddress(){
+    //1.获取权限状态
+    wx.getSetting({
+      success: function(result) {
+        const scopeAddress = result.authSetting['scope.address'];
+        if(scopeAddress == true || scopeAddress === undefined){
+          wx.chooseAddress({
+            success: (result)=>{
+              wx.setStorageSync('address',result)
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+          });
+        }else{
+          wx.openSetting({
+            success:(result2)=>{
+              wx.chooseAddress({
+                success:(result3)=>{
+                  console.log(result3)
+                }
+              })
+            }
+          })
+        }
+      }
+    })
   },
-
+  data: {
+    address:{}
+  },
+  onShow: function () {
+    let address = wx.getStorageSync('address')
+    address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo
+    this.setData({
+      address
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -25,9 +67,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
 
-  },
 
   /**
    * 生命周期函数--监听页面隐藏
